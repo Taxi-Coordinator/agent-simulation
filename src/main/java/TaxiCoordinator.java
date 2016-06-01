5e252f5f150184aed4789e96e15cf67961b3915c
Jherez Taylor <jherez.taylor@gmail.com>
2016-06-01 23:33:15 +0800

    Added dijkstra implementation - Sourced from Algorithms 4th Ed. Princeton University

--- src/main/java/TaxiCoordinator.java
+++ src/main/java/TaxiCoordinator.java
@@ -1,13 +1,39 @@
-import utils.RandomNumGen;
+import utils.libs.In;
+import utils.libs.StdOut;
+import utils.shortestPath.DijkstraSP;
+import utils.shortestPath.DirectedEdge;
+import utils.shortestPath.EdgeWeightedDigraph;
 
 public class TaxiCoordinator {
     public static void main(String[] args) {
-        System.out.println("Hello Taxi");
-        int N = 15;
-        double mean = 2.0;
-        double stddev = 1.5;
-        for(int i = 1; i <= N; i++) {
-            System.out.println(RandomNumGen.gaussian(mean,stddev));
+//        System.out.println("Hello Taxi");
+//        int N = 15;
+//        double mean = 2.0;
+//        double stddev = 1.5;
+//        for(int i = 1; i <= N; i++) {
+//            System.out.println(RandomNumGen.gaussian(mean,stddev));
+//        }
+
+        In in = new In("src/main/resources/input.txt");
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+        int s = Integer.parseInt("1");
+
+        // compute shortest paths
+        DijkstraSP sp = new DijkstraSP(G, s);
+
+
+        // print shortest path
+        for (int t = 0; t < G.V(); t++) {
+            if (sp.hasPathTo(t)) {
+                StdOut.printf("%d to %d (%.2f)  ", s, t, sp.distTo(t));
+                for (DirectedEdge e : sp.pathTo(t)) {
+                    StdOut.print(e + "   ");
+                }
+                StdOut.println();
+            }
+            else {
+                StdOut.printf("%d to %d         no path\n", s, t);
+            }
         }
     }
 }
