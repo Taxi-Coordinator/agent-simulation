3d918d8a1b0c8c3cf35d04d7c0f5ef724b432a1e
Jherez Taylor <jherez.taylor@gmail.com>
2016-06-02 10:01:14 +0800

    removed old randomNumber class

--- src/main/java/TaxiCoordinator.java
+++ src/main/java/TaxiCoordinator.java
@@ -1,18 +1,19 @@
 import utils.libs.In;
 import utils.libs.StdOut;
+import utils.libs.StdRandom;
 import utils.shortestPath.DijkstraSP;
 import utils.shortestPath.DirectedEdge;
 import utils.shortestPath.EdgeWeightedDigraph;
 
 public class TaxiCoordinator {
     public static void main(String[] args) {
-//        System.out.println("Hello Taxi");
-//        int N = 15;
-//        double mean = 2.0;
-//        double stddev = 1.5;
-//        for(int i = 1; i <= N; i++) {
-//            System.out.println(RandomNumGen.gaussian(mean,stddev));
-//        }
+        System.out.println("Hello Taxi");
+        int N = 15;
+        double mean = 2.0;
+        double stddev = 1.5;
+        for(int i = 1; i <= N; i++) {
+            System.out.println(StdRandom.gaussian(mean,stddev));
+        }
 
         In in = new In("src/main/resources/v_city.txt");
         EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
