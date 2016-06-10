fe4906a3c278b7869a32e179c4ab21b566937910
eduardo-salazar <esalazar922@gmail.com>
2016-06-11 02:13:41 +0800

    Clean intersection class. Moved method to Main class and not intersection

--- src/main/java/city/Intersection.java
+++ src/main/java/city/Intersection.java
@@ -6,10 +6,8 @@ import java.util.Date;
 import java.util.List;
 
 public class Intersection {
-    public Date nextTime;
     public int calls = 0;
     public int index;
-    final static int MAX_CALLS = 4;
 
     public List<Integer> connections = new ArrayList<Integer>();
 
@@ -21,12 +19,6 @@ public class Intersection {
     public Intersection() {
     }
 
-//    public void nextTime(Date currentTime){
-//        if(calls < MAX_CALLS)
-//            this.nextTime = Helper.nextCall(currentTime);
-//        else
-//            this.nextTime = null;
-//    }
 
     public void receiveCall() {
         this.calls++;
